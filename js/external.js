8cdd56349950c19ffd9d9450b5123ec8922bfebe
Michael Friend <michael.l.friend2@gmail.com>
2022-04-13 15:20:49 -0500

    Finished JavaScript with HTML Exercises

--- js/external.js
+++ js/external.js
@@ -11,3 +11,21 @@ let hercules = prompt("How long will you be renting Hercules for?");
 let pricePerDay = 3
 let priceOfRental = alert((pricePerDay * littleMermaid) + (pricePerDay * brotherBear) + (pricePerDay * hercules));
 
+let googlePay = prompt("What is your hourly rate at Google?");
+let amazonPay = prompt("What is your hourly rate at Amazon");
+let facebookPay = prompt("What is your hourly rate at Facebook");
+let googleHours = prompt("How many hours did you work at Google this week?");
+let amazonHours = prompt("How many hours did you work at Amazon this week?");
+let facebookHours = prompt("How many hours did you work at Facebook this week?");
+let totalOfPaycheck = alert((googlePay * googleHours) + (amazonPay * amazonHours) + (facebookPay * facebookHours));
+
+let full = confirm("Is this class full?");
+let conflict = confirm("Does the class schedule conflict with your schedule?");
+let enroll = alert(!full && !conflict);
+
+let member = confirm("Is this customer a Premium Member?");
+let amount = confirm("Are they purchasing 2 or more items?");
+let expired = confirm("Is this offer expired?");
+let apply = alert((member && !expired) || (amount && !expired));
+
+
