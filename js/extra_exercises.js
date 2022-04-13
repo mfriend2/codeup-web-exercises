cf32b2be1db75e29ed3101919bf5923350602596
Michael Friend <michael.l.friend2@gmail.com>
2022-04-13 17:00:40 -0500

    extra JS HTML exercises

---
+++ js/extra_exercises.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let name = prompt("What is your name");
+let stringCount = name.length;
+alert(name + " contains " + stringCount + " characters.");
+
+let noun = prompt ("Please enter a noun:");
+let verb = prompt("Please enter a verb:");
+let adjective = prompt("Please enter an adjective:");
+let adverb = prompt("Please enter an adverb:");
+alert("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + " " + " ? ");
+
