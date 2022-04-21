070784910ccebe308805d8ab46bda044fd0b7dc9
Michael Friend <michael.l.friend2@gmail.com>
2022-04-21 14:59:51 -0500

    completed break_and_continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,27 @@
+"use strict";
+(function () {
+    function numIsOdd() {
+        let userOddNum;
+        while (true) {
+            userOddNum = parseFloat(prompt("Enter an odd number 1-50."));
+            if (userOddNum % 2 === 0) {
+                continue;
+            }
+            break;
+        }
+        console.log("Number to skip is " + userOddNum);
+        for (let x = 0; x < 50; x++) {
+            if (x % 2 === 0) {
+                continue;
+            } else if (x === userOddNum) {
+                console.log("Skipping this number: " + userOddNum);
+            } else {
+                console.log("Here is an odd number: " + x);
+            }
+        }
+    }
+
+    numIsOdd();
+
+
+})()
