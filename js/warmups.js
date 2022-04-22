a1a509585b73a638d41790d9737364298b7557e0
Michael Friend <michael.l.friend2@gmail.com>
2022-04-22 09:15:34 -0500

    finished warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,3 +1,14 @@
 "use strict"
 
 console.log("Yay! I completed the warm up!");
+
+for (let x = 1; x < 101; x++){
+    if (x % 3 === 0 &&  x % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if ( x % 3 === 0) {
+        console.log("Fizz");
+    } else if (x % 5 === 0 ) {
+        console.log("Buzz");
+    }
+    console.log(x);
+}
