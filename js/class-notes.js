be5a31a27201d406a820435a6f2ad8f80cfac80d
Michael Friend <michael.l.friend2@gmail.com>
2022-04-22 10:33:32 -0500

    class notes

--- js/class-notes.js
+++ js/class-notes.js
@@ -1,8 +1,24 @@
 "use strict";
-(function() {
- /*
+(function () {
 
+    //  HOW TO BUILD A NUMBER PYRAMID   \\
 
-  */
+    // FIRST WAY
+    /* for (let x = 1; x < 10; x++) {
+         let str = x.toString().repeat(x);
+         console.log(str);
+     }   */
+
+    // SECOND WAY
+    /* function numPyr() {
+         for (let y = 1; y < 10; y++){
+             let str = "";
+             for (let x = 1; x <= y; x++) {
+                 str += y;
+             }
+             console.log(str);
+         }
+     }
+     numPyr();   */
 
 })();
