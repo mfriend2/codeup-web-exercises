1446c915de04969673dde90a4b13a6c50ec19305
Michael Friend <michael.l.friend2@gmail.com>
2022-05-09 09:29:31 -0500

    completed todays warmup

--- js/warmups.js
+++ js/warmups.js
@@ -80,3 +80,49 @@ function returnAllProductEssentialDetails (arrObj) {
     }
 }
 returnAllProductEssentialDetails([product1, product2, product3]);
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function tallestObj (arr) {
+        let tallest = {heightInMM: 0};
+        for (let hamster of arr) {
+            if (hamster.heightInMM > tallest.heightInMM) {
+                tallest = hamster;
+            }
+        }
+    return tallest;
+}
+
+console.log(tallestObj(hamsters));
