9131d8b87b745cba5b89fcaf4f7cb50f241130bd
Michael Friend <michael.l.friend2@gmail.com>
2022-05-25 13:52:08 -0500

    updated

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -3,7 +3,7 @@ $.get('/data/blog.json').done(function (data) {
     $('#posts').html('');
     data.forEach(function (el) {
         let card = '<div class="card">'
-        let title = '<h5 class="card-header">' + el.title + '</h5>';
+        let title = '<h5 class="card-header bg-dark text-white">' + el.title + '</h5>';
         let content = '<div class="card-body">' + '<p class="card-text">' + el.content + '</p>';
         let categories = '<p class="fw-light">' + el.categories + '</p>';
         let date = '<footer class="blockquote-footer">' + 'Date Posted: ' + el.date + '</footer>' + '</div>' + '</div>';
