9013830083f8a5bb1dc9c5d8eaeca9af1db324d0
Michael Friend <michael.l.friend2@gmail.com>
2022-05-25 12:27:12 -0500

    updated

---
+++ js/ajax-blog.js
@@ -0,0 +1,12 @@
+$.get('/data/blog.json').done(function (data) {
+    console.log(data);
+    $('#posts').html('');
+    data.forEach(function (el) {
+        let card = '<div class="card">'
+        let title = '<h5 class="card-header">' + el.title + '</h5>';
+        let content = '<div class="card-body">' + '<p class="card-text">' + el.content + '</p>';
+        let categories = '<p class="fw-light">' + el.categories + '</p>';
+        let date = '<footer class="blockquote-footer">' + 'Date Posted: ' + el.date + '</footer>' + '</div>' + '</div>';
+        $('#posts').append(card + title + content + categories + date);
+    })
+});
